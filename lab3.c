f55468d5072db7b69c64756c6c78ae56e840f630
lomalovo2004 <shulgae2004@gmail.com>
2023-10-09 17:23:02 +0300

    try3
    ura

--- lab3.c
+++ lab3.c
@@ -10,34 +10,54 @@ void quadratic_equation(double a, double b, double c) {
         double root2 = (-b - sqrt(discriminant)) / (2 * a);
         
         printf("Roots: %.2f, %.2f\n", root1, root2);
-    } else if (discriminant == 0) {
+    } 
+    else if (discriminant == 0) {
         double root = -b / (2 * a);
         
         printf("Root: %.2f\n", root);
-    } else {
+    } 
+    else {
         printf("No real roots\n");
     }
 }
 
+void permutations(double a, double b, double c, double epsilon) {
+    quadratic_equation(a, b, c);
+    quadratic_equation(a, c, b);
+    quadratic_equation(b, a, c);
+    quadratic_equation(b, c, a);
+    quadratic_equation(c, a, b);
+    quadratic_equation(c, b, a);
+}
+
 void check_divisibility(int num1, int num2) {
+    if (num2 == 0){
+        printf("It is impossible to divide by 0\n");
+        return;
+    }
+    
     if (num2 != 0 && num1 % num2 == 0) {
         printf("%d is divisible by %d\n", num1, num2);
-    } else {
+    } 
+    else {
         printf("%d is not divisible by %d\n", num1, num2);
     }
 }
 
 void check_triangle(double a, double b, double c) {
     if (a > 0 && b > 0 && c > 0) {
-        if (fabs(a * a + b * b - c * c) < 0.0001 || fabs(a * a + c * c - b * b) < 0.0001 || fabs(b * b + c * c - a * a) < 0.0001) {
+        if (fabs(a * a + b * b - c * c) < 0.0001 && fabs(a * a + c * c - b * b) < 0.0001 && fabs(b * b + c * c - a * a) < 0.0001) {
             printf("The given lengths can form a right triangle\n");
         } else {
             printf("The given lengths cannot form a right triangle\n");
         }
-    } else {
+    } 
+    else {
         printf("Invalid side lengths\n");
     }
 }
+
+
 
 int main(int argc, char *argv[]) {
     if (argc < 2) {
@@ -55,41 +75,40 @@ int main(int argc, char *argv[]) {
             }
             
             double epsilon;
-            double coef1;
-            double coef2;
-            double coef3;
+            double coefficients[3];
             
             if (sscanf(argv[2], "%lf", &epsilon) != 1 ||
-                sscanf(argv[3], "%lf", &coef1) != 1 ||
-                sscanf(argv[4], "%lf", &coef2) != 1 ||
-                sscanf(argv[5], "%lf", &coef3) != 1) {
+                sscanf(argv[3], "%lf", &coefficients[0]) != 1 ||
+                sscanf(argv[4], "%lf", &coefficients[1]) != 1 ||
+                sscanf(argv[5], "%lf", &coefficients[2]) != 1) {
                 printf("Invalid parameter type for flag -q\n");
                 return 1;
             }
             
-            quadratic_equation(coef1, coef2, coef3);
+            permutations(coefficients[0], coefficients[1], coefficients[2], epsilon);
             break;
             
         case 'm':
             if (argc != 4) {
-                printf("Invalid number of parameters for flag -m\n");
+                printf("Invalid number of parameters for flag -d\n");
                 return 1;
             }
             
-            double num1;
-            double num2;
+            int num1;
+            int num2;
             
-            if (sscanf(argv[2], "%lf", &num1) != 1 || sscanf(argv[3], "%lf", &num2) != 1) {
-                printf("Invalid parameter type for flag -m\n");
+            if (sscanf(argv[2], "%d", &num1) != 1 || sscanf(argv[3], "%d", &num2) != 1) {
+                printf("Invalid parameter type for flag -d\n");
                 return 1;
             }
             
+
             if (floor(num1) != num1 || floor(num2) != num2) {
                 printf("Invalid parameter type for flag -m\n");
                 return 1;
             }
-            
-            check_divisibility((int)num1, (int)num2);
+
+            check_divisibility(num1, num2);
             break;
             
         case 't':
@@ -98,15 +117,13 @@ int main(int argc, char *argv[]) {
                 return 1;
             }
             
-            double epsilon2;
             double side1;
             double side2;
             double side3;
             
-            if (sscanf(argv[2], "%lf", &epsilon2) != 1 ||
-                sscanf(argv[3], "%lf", &side1) != 1 ||
-                sscanf(argv[4], "%lf", &side2) != 1 ||
-                sscanf(argv[5], "%lf", &side3) != 1) {
+            if (sscanf(argv[2], "%lf", &side1) != 1 ||
+                sscanf(argv[3], "%lf", &side2) != 1 ||
+                sscanf(argv[4], "%lf", &side3) != 1) {
                 printf("Invalid parameter type for flag -t\n");
                 return 1;
             }
---
+++ lab7.c
@@ -0,0 +1,137 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+void writeToFile(char* filename, char* file1, char* file2, char* file3) {
+    FILE* output = fopen(filename, "w");
+    if (output == NULL) {
+        printf("Ошибка открытия файла\n");
+        return;
+    }
+
+    FILE* input1 = fopen(file1, "r");
+    FILE* input2 = fopen(file2, "r");
+    FILE* input3 = fopen(file3, "r");
+    if (input1 == NULL || input2 == NULL || input3 == NULL) {
+        printf("Ошибка открытия входных файлов\n");
+        fclose(output);
+        return;
+    }
+
+    char lexeme[100];
+    int count1 = 0, count2 = 0, count3 = 0;
+    int position = 1;
+
+    while (1) {
+        if (position % 2 == 1) {
+            if (fscanf(input2, "%s", lexeme) != EOF) {
+                fprintf(output, "%s ", lexeme);
+                count2++;
+            } else {
+                break;
+            }
+        } else {
+            if (fscanf(input3, "%s", lexeme) != EOF) {
+                fprintf(output, "%s ", lexeme);
+                count3++;
+            } else {
+                break;
+            }
+        }
+
+        position++;
+    }
+
+    rewind(input1);
+    while (fscanf(input1, "%s", lexeme) != EOF) {
+        fprintf(output, "%s ", lexeme);
+        count1++;
+    }
+
+    fclose(input1);
+    fclose(input2);
+    fclose(output);
+}
+
+void transformFile(char* filename, char* input) {
+    FILE* output = fopen(filename, "w");
+    if (output == NULL) {
+        printf("Ошибка открытия файла\n");
+        return;
+    }
+
+    FILE* input_file = fopen(input, "r");
+    if (input_file == NULL) {
+        printf("Ошибка открытия входного файла\n");
+        fclose(output);
+        return;
+    }
+
+    char lexeme[100];
+    int count = 0;
+
+    while (fscanf(input_file, "%s", lexeme) != EOF) {
+        if (count % 10 == 9) {
+            for (int i = 0; i < strlen(lexeme); i++) {
+                if (isalpha(lexeme[i])) {
+                    lexeme[i] = tolower(lexeme[i]);
+                    lexeme[i] = '0' + (tolower(lexeme[i]) - 'a');
+                }
+            }
+        } else if (count % 2 == 1) {
+            for (int i = 0; i < strlen(lexeme); i++) {
+                if (isalpha(lexeme[i])) {
+                    lexeme[i] = tolower(lexeme[i]);
+                }
+            }
+        } else if (count % 5 == 4) {
+            int num = atoi(lexeme);
+            sprintf(lexeme, "%o", num);
+        }
+
+        fprintf(output, "%s ", lexeme);
+        count++;
+    }
+
+    fclose(input_file);
+    fclose(output);
+}
+
+int main(int argc, char* argv[]) {
+    if (argc < 4) {
+        printf("Недостаточно аргументов\n");
+        return 1;
+    }
+
+    char* flag = argv[1];
+    char* filename = argv[2];
+
+    if (strcmp(flag, "-r") == 0) {
+        if (argc != 5) {
+            printf("Некорректное количество аргументов для флага -r\n");
+            return 1;
+        }
+
+        char* file1 = argv[2];
+        char* file2 = argv[3];
+        char* file3 = argv[4];
+        writeToFile(filename, file1, file2, file3);
+    } 
+    else if (strcmp(flag, "-a") == 0) {
+        if (argc != 4) {
+            printf("Некорректное количество аргументов для флага -a\n");
+            return 1;
+        }
+
+        char* input = argv[3];
+        transformFile(filename, input);
+    } 
+    else {
+        printf("Некорректный флаг\n");
+        return 1;
+    }
+
+    printf("Программа выполнена успешно\n");
+    return 0;
+}
